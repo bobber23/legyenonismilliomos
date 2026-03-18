19f8275c80656934d8dc5c4a863b7c4faf1a639f
Botond Bugja <66123555+bobber23@users.noreply.github.com>
2026-03-18 13:24:56 +0100

    Update game.js

--- frontend/javascript/game.js
+++ frontend/javascript/game.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // loginCheck();
+    loginCheck();
     kerdes();
     levelszinezes();
     document.getElementById('half').addEventListener('click', felezo);
